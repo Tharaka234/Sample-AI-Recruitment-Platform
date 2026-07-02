992b2b85b4dd37495246056c86708aefdf090a2b
G T Thushan <theekshanathushan89@gmail.com>
2026-07-02 21:30:24 +0530

    I add new animations,photoes my site

--- RecruitmentPlatform.API/obj/Debug/net10.0/RecruitmentPlatform.API.AssemblyInfo.cs
+++ RecruitmentPlatform.API/obj/Debug/net10.0/RecruitmentPlatform.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecruitmentPlatform.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44b722c0b76fc0db55b1378fd3c82796c54e7db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccba429219b7c0eb78a3a4afe68b76e549fa90ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecruitmentPlatform.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecruitmentPlatform.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
